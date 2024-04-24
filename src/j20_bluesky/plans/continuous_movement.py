a5d5981694af8f11ae41a4d1146c2c1bc4429e0a
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-04-24 08:34:09 +0000

    some tests

--- src/j20_bluesky/plans/continuous_movement.py
+++ src/j20_bluesky/plans/continuous_movement.py
@@ -1,6 +1,6 @@
 from typing import Any, Mapping, Optional
 
-from bluesky import MsgGenerator
+from dls_bluesky_core.core import MsgGenerator, inject
 
 
 def continuous_movement(metadata: Optional[Mapping[str, Any]] = None) -> MsgGenerator:
---
+++ tests/test_xturbo_slit_movement.py
@@ -0,0 +1,9 @@
+from dodal.beamlines import i20_1
+
+device = i20_1.device(fake_with_ophyd_sim=True)
+
+
+def test_direct_movement():
+    test_sim_device = OphydDevice()
+    result = await  j20_bluesky.direct_movement(test_sim_device)
+
